cc417346231d010e523a6ce382cb09df0f90dbb7
JoaoVSene <joaovictor_sene@hotmail.com>
2024-12-07 09:35:48 -0300

    update team members

--- components/sections/Team.tsx
+++ components/sections/Team.tsx
@@ -3,17 +3,17 @@ import Image from "next/image";
 
 const team = [
   {
-    name: "Pedro Augusto Ribeiro Gomes",
+    name: "Pedro Gomes",
     role: "Fundador & Presidente",
     image: "https://media.licdn.com/dms/image/v2/D4E03AQHzRuyQbeX4lw/profile-displayphoto-shrink_200_200/profile-displayphoto-shrink_200_200/0/1718288174193?e=1738195200&v=beta&t=XMxCUWaJOyf7XLKg2WFDdprIV9Tap6XxT0Gff5A-VzE",
   },
   {
-    name: "Pedro Kenzo Muramatsu ",
+    name: "Pedro Kenzo",
     role: "Cofundador",
     image: "https://media.licdn.com/dms/image/v2/D4D03AQFR5VFZlJcn1g/profile-displayphoto-shrink_200_200/profile-displayphoto-shrink_200_200/0/1707251060763?e=1738800000&v=beta&t=OY0ORZj3CKZc-VjxG82E0zp4MeZMOxc-cHNTQTFjjPg",
   },
   {
-    name: "Pedro Augusto Monteiro Delgado",
+    name: "Pedro Monteiro",
     role: "Diretor de Projetos",
     image: "https://media.licdn.com/dms/image/v2/D4D03AQGOLuJrkBEHWg/profile-displayphoto-shrink_100_100/profile-displayphoto-shrink_100_100/0/1710188752172?e=1738800000&v=beta&t=DEapMTUShwArwQaG773_mU496uR59AEx9uqMDoCEBiE",
   },
@@ -23,19 +23,29 @@ const team = [
     image: "https://media.licdn.com/dms/image/v2/D4D03AQHT_lQbzVtm6Q/profile-displayphoto-shrink_100_100/profile-displayphoto-shrink_100_100/0/1704713329933?e=1738800000&v=beta&t=1ySxh9nSzZc-4Ue9hAoFn5jKAstVAaTCrg2L6l2Z77Q",
   },
   {
-    name: "Gustavo Sampaio Lima",
+    name: "Gustavo Sampaio",
     role: "Diretor de Projetos",
     image: "https://media.licdn.com/dms/image/v2/D4D03AQE0i9G3l4VsEw/profile-displayphoto-shrink_100_100/profile-displayphoto-shrink_100_100/0/1709077635249?e=1738800000&v=beta&t=IP0A7ZopccY7IkJXK9DYaANTu6ZvsNrEYmtyVpOMenU",
   },
   {
-    name: "Gabriel Moraes Andreghetti",
+    name: "Gabriel Andreghetti",
     role: "Diretor de Projetos",
     image: "https://media.licdn.com/dms/image/v2/D4E03AQF9I_28nZpSgA/profile-displayphoto-shrink_100_100/profile-displayphoto-shrink_100_100/0/1684849954299?e=1738800000&v=beta&t=EYLfQumty-GVpALGeyHMKQv7LK4tNI2iwUdmalgg3iY",
   },
   {
-    name: "João Pedro Alonso Almeida",
+    name: "João Alonso",
     role: "Diretor de Marketing",
     image: "https://media.licdn.com/dms/image/v2/D4D03AQHmoKNHM9nYHg/profile-displayphoto-shrink_100_100/profile-displayphoto-shrink_100_100/0/1727298079285?e=1738800000&v=beta&t=LDaVCHe8DnAB6x8J9-kaEBWi9IHIE0wqWAMBdE1omWI",
+  },
+  {
+    name: "Gustavo Ribeiro",
+    role: "Diretor de Marketing",
+    image: "https://media.licdn.com/dms/image/v2/D4E03AQGk5HD6nB90Vw/profile-displayphoto-shrink_100_100/profile-displayphoto-shrink_100_100/0/1700336561356?e=1738800000&v=beta&t=kZs94vz7MwYenVy0hRjh8KVcc1re_GUBGJyD34nJdXw",
+  },
+  {
+    name: "João Sene",
+    role: "Diretor de Tech",
+    image: "https://media.licdn.com/dms/image/v2/D4D03AQEZWRp_ocjv_Q/profile-displayphoto-shrink_200_200/profile-displayphoto-shrink_200_200/0/1712750093808?e=1738800000&v=beta&t=SMEw5ChJuZfh09jyVCmq6FVz5rATiPm4qYNwFveQBHI",
   },
 ];
 
